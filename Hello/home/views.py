adadcb95f7b1c49a9f0ba01f1ea6a75bedcde8d2
aortamunu <seemi1911@gmail.com>
2023-04-18 22:21:41 +0545

    template inheritance

---
+++ Hello/home/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+def index(request):
+    context = {
+        "variable1":"Seemana is kind",
+        "variable2":"She is brave"
+    }
+    return render(request, 'index.html', context)
+    # return HttpResponse("this is homepage")
+
+def about(request):
+    return HttpResponse("this is about page")
+
+def services(request):
+    return HttpResponse("this is services page")
+
+def contact(request):
+    return HttpResponse("this is contact page")
